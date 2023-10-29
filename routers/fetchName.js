e29f5450ebe3806871e65ee10813266265b3d525
ShreeramNew <shreerambca1@gmail.com>
2023-10-29 12:21:26 +0530

    refacor- routers code are refactored

--- routers/fetchName.js
+++ routers/fetchName.js
@@ -2,23 +2,29 @@ const express = require("express");
 const router = express.Router();
 const Names = require("../models/Name");
 
-router.post("/", (req, res) => {
+// Handles POST request for "http://localhost:3000/names"
+router.post("/", async (req, res) => {
    const data = req.body;
-   Names.create({
-      name: data.name,
-      greeting: data.greeting,
-   })
-      .then(() => res.send("Data is saved!"))
-      .catch((err) => res.send(err));
+   try {
+      await Names.create({
+         name: data.name,
+         greeting: data.greeting,
+      });
+      res.send("Data is saved!");
+   } catch (err) {
+      res.send(err);
+   }
 });
+
+
+// Handles GET request for "http://localhost:3000/names"
 router.get("/", async (req, res) => {
-    try{
-         let names= await Names.find({})
-         res.json(names);
-    }
-    catch(errors){
-        res.send(errors)
-    }
+   try {
+      let names = await Names.find({});
+      res.json(names);
+   } catch (errors) {
+      res.send(errors);
+   }
 });
 
 module.exports = router;
--- routers/jsr.js
+++ routers/jsr.js
@@ -3,40 +3,41 @@ const router = express.Router();
 const login = require("../models/Login");
 const { validationResult, body } = require("express-validator");
 
+//Checks if name is email or not and Bhrutya is in Uppercase
 const ValidateInputs = [
    body("name").isEmail().withMessage("Name should be email"),
    body("Bhrutya").isUppercase().withMessage("Bhrutya value should be uppercase"),
 ];
 
-//Handling POST request
-router.post("/", ValidateInputs, (req, res) => {
+//Handling POST request for "http://localhost:3000/jsr"
+router.post("/", ValidateInputs, async (req, res) => {
    const errors = validationResult(req);
    if (!errors.isEmpty()) {
-      return res.send(errors);
+      return res.send(errors); //Returns if Validation conditions are not satisified
    }
    const userData = req.body;
-   login
-      .create({
+   try {
+      await login.create({
          email: userData.name,
          password: userData.Bhrutya,
-      })
-      .then(() => {
-         res.send("Bolo siya var Rama Chandra ji Ki...jai");
-      })
-      .catch((err) => res.send(err));
+      });
+      res.send("Bolo siya var Rama Chandra ji Ki...jai");
+   } catch (err) {
+      res.send(err);
+   }
 });
 
-//Handling GET request
-router.get("/", (req, res) => {
-   login
-      .find({
-       password: { $eq: "Jai Hanuman" } 
-      })
-      .then((logins) => {
-         res.json(logins);
-         console.log(typeof(logins));
-      })
-      .catch((err) => res.send(err));
+//Handling GET request for "http://localhost:3000/jsr"
+router.get("/", async (req, res) => {
+   try {
+      let logins = await login.find({
+         password: { $eq: "Jai Hanuman" },
+      });
+
+      logins = res.json(logins);
+   } catch (err) {
+      res.send(err);
+   }
 });
 
 module.exports = router;
